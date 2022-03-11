5386d0c6993ed5468ce881500797126a06c90fca
Jack Linke <jacklinke@gmail.com>
2022-03-10 22:39:57 -0500

    Make sure migrations have been run before adding superuser

--- tests/example/core/apps.py
+++ tests/example/core/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
+from django.db import DEFAULT_DB_ALIAS, connections
+from django.db.migrations.executor import MigrationExecutor
+
+
+def is_database_synchronized(database):
+    connection = connections[database]
+    connection.prepare_database()
+    executor = MigrationExecutor(connection)
+    targets = executor.loader.graph.leaf_nodes()
+    return not executor.migration_plan(targets)
 
 
 class CoreConfig(AppConfig):
@@ -7,7 +17,8 @@ class CoreConfig(AppConfig):
     name = "tests.example.core"
 
     def ready(self) -> None:
-        User = get_user_model()
-        user_exists = User.objects.filter(username="admin", email="admin@example.com").exists()
-        if not user_exists:
-            User.objects.create_superuser("admin", "admin@example.com", "pass")
+        if is_database_synchronized(DEFAULT_DB_ALIAS):
+            User = get_user_model()
+            user_exists = User.objects.filter(username="admin", email="admin@example.com").exists()
+            if not user_exists:
+                User.objects.create_superuser("admin", "admin@example.com", "pass")
