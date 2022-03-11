9acd78250155ff33b1919ad9829df4cf2e004c44
Jack Linke <jacklinke@gmail.com>
2022-03-10 22:12:58 -0500

    Add admin to example project

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.conf import settings
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path(settings.ADMIN_URL, admin.site.urls),
     path("", include("django_directed.urls", namespace="django_directed")),
 ]
